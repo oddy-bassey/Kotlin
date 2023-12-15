6e7f732fb81d384b4ba936e94d1c0218b58c251c
oddy-bassey <oddybassey@gmail.com>
2023-12-15 03:56:58 +0100

    numeric datatypes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,31 @@
+/* Note:
+ * By default Integer types that fit within 32 bits will
+ * have an inferred type of Int but if it's larger than
+ * 32bits then it's inferred as Long
+ *
+ * The inferred type of floating point number will be a Double
+ */
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val num1: Int = 23 // 32bits
+    val num3: Byte = 2 // 8bits
+    val num4: Short = 4 // 16bits
+    val num5: Long = 33 // 64bits
 
-    // Try adding program arguments via Run/Debug configuration.
-    doSomething();
-    println("Program arguments: ${args.joinToString()}")
-}
+    val num2 = 78 // (type inference) inferred as Int
+    val num6 = 100000000000 // type inference, inferred as Long
 
-fun doSomething() {
-    var firstName = "Odudu"
-    val lastName = "Bassey"
+    val num7 = 10L
+    val num8: UInt = 21u
+    val num9: ULong = 23UL
 
-    print("This is me ${lastName} ${firstName} learning KOTLIN!!");
+    val num10 = 6.45 // inferred to Double
+    val num11 = 3.3f // inferred to Float
+
+    // Numeric type conversion
+    println(233.4.toInt())
+    println(233.4.toFloat())
+    println(233.654.toULong())
+    println(100_000_000.toByte())
+    println(123_456_789.toByte())
 }
