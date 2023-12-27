3109aab5718ff9521180b2c8b7b2d33f81137de2
oddy-bassey <oddybassey@gmail.com>
2023-12-27 07:50:07 +0100

    try/catch as control flow

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,13 @@
-/* Note:
- * By default Integer types that fit within 32 bits will
- * have an inferred type of Int but if it's larger than
- * 32bits then it's inferred as Long
- *
- * The inferred type of floating point number will be a Double
- */
 
 fun main(): Unit {
-    val message1 = getMessageWithIf(32)
-    println(message1)
+    val message = try {
+        throw IllegalStateException()
+        "the value is ${100 / 0}"
+    } catch (error: IllegalStateException) {
+        "An illegal state exception occurred"
+    } catch (error: ArithmeticException) {
+        "An arithmetic exception occurred"
+    }
 
-    val message2 = getMessageWithWhen(85)
-    println(message2)
-}
-
-fun getMessageWithIf(value: Int) = if (value>50) {
-    "pipeline is overloaded"
-} else {
-    "pipeline in stable condition"
-}
-
-fun getMessageWithWhen(value: Int) =  when {
-    value > 50 -> "pipeline overload"
-    else -> "pipeline in stable condition"
+    println(message)
 }
