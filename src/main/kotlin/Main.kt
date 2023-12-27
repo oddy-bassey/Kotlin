7ad5262dd599c952719745f92ad2419a1027bffb
oddy-bassey <oddybassey@gmail.com>
2023-12-27 08:12:26 +0100

    smart casting

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,29 @@
 
 fun main(): Unit {
-    val message = try {
-        throw IllegalStateException()
-        "the value is ${100 / 0}"
-    } catch (error: IllegalStateException) {
-        "An illegal state exception occurred"
-    } catch (error: ArithmeticException) {
-        "An arithmetic exception occurred"
-    }
+    val value: Any = "32"
 
-    println(message)
+    checkTypeWithIf(value)
+    checkInputWithWhen(null)
+}
+
+fun checkTypeWithIf(input: Any?) {
+     if (input is String) {
+         if (input == null) {
+             println("input string is null")
+         } else {
+             println("input is a String with length ${input.length}")
+         }
+     }
+
+    if (input is Int) {
+        println("input is an Int")
+    }
+}
+
+fun checkInputWithWhen(input: Any?) {
+    when (input) {
+        null -> println("input is a null value")
+        is String -> println("input is a String with length ${input.length}")
+        is Int -> println("input is an Int")
+    }
 }
