b250adcc6e071e7439b675d3db3b45da8be3e8fd
oddy-bassey <oddybassey@gmail.com>
2024-01-13 01:04:48 +0100

    Arrays

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,13 +6,37 @@
  * - in
  */
 fun main(): Unit {
-    println("For Loop")
+    println("Arrays")
 
-    for(counter in 0 until 5) println(counter)
-    println()
-    for(counter in 10 downTo 0) println(counter)
-    println()
-    for(counter in 10 downTo 0 step 2) println(counter)
-    println()
-    for(counter in 20 .. 30) println(counter)
+    val numbers = arrayOf(1, 2, 3, null)
+    val nullArray = arrayOfNulls<Int>(5)
+    val custom = Array(5) {index -> index}
+
+    val size = numbers.size
+    val value1 = numbers.get(1)
+    val value2 = nullArray[0]
+    nullArray.set(0, 21)
+    numbers[3] = 32
+
+    val array2 = intArrayOf(1, 2, 3, 4, 5)
+    val primitiveShort = ushortArrayOf(1u, 2u, 3u, 4u)
+
+    for(i in 0 until array2.size) {
+        println(array2[i])
+    }
+    println("Second ---------------- ")
+    for(element in primitiveShort) {
+        println(element)
+    }
+    println("Third ---------------- ")
+    nullArray.forEach { element -> println(element) }
+
+    println("Fourth ---------------- ")
+    greeting("Hello:)", "David", "Solomon", "Sasa")
+}
+
+fun greeting(greeting: String, vararg names: Any) {
+    for(name in names) {
+        println("$greeting, $name")
+    }
 }
