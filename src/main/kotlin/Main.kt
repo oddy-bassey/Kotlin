ada2ad7bb51e7ed114f5c3e0791b4120533cfa08
oddy-bassey <oddybassey@gmail.com>
2024-01-13 13:45:43 +0100

    lists (immutable and mutable)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,37 +6,31 @@
  * - in
  */
 fun main(): Unit {
-    println("Arrays")
+    println("List")
 
-    val numbers = arrayOf(1, 2, 3, null)
-    val nullArray = arrayOfNulls<Int>(5)
-    val custom = Array(5) {index -> index}
-
-    val size = numbers.size
-    val value1 = numbers.get(1)
-    val value2 = nullArray[0]
-    nullArray.set(0, 21)
-    numbers[3] = 32
-
-    val array2 = intArrayOf(1, 2, 3, 4, 5)
-    val primitiveShort = ushortArrayOf(1u, 2u, 3u, 4u)
-
-    for(i in 0 until array2.size) {
-        println(array2[i])
+    val list1: List<String> = listOf("debby", "mango", "rice");
+    val list2 = listOf(1, 2, 3, null)
+    val list3 = List<Int>(5) {index ->
+        index * 2
     }
-    println("Second ---------------- ")
-    for(element in primitiveShort) {
-        println(element)
-    }
-    println("Third ---------------- ")
-    nullArray.forEach { element -> println(element) }
+    list3.forEach { println(it) }
+    val emptyList: List<Int> = emptyList();
+    val firstValue = list3.get(0)
+    val secondValue = list3[1]
 
-    println("Fourth ---------------- ")
-    greeting("Hello:)", "David", "Solomon", "Sasa")
-}
+    val languages = mutableListOf<String>("Java", "Kotlin", "C#", "Php");
+    languages.set(0, "JAVA")
+    languages[languages.size-1] = "PHP"
 
-fun greeting(greeting: String, vararg names: Any) {
-    for(name in names) {
-        println("$greeting, $name")
-    }
+    languages.add("Javascript")
+    languages.add("C++")
+
+    languages.removeAt(2)
+    languages.remove("PHP")
+
+    languages.forEach{ println(it) }
+
+    // type compatibility
+    val data: List<String> = mutableListOf("dog", "fish", "cat")
+    // data[0] = "we": this will not work because the mutable list has now been assigned to an immutable list
 }
