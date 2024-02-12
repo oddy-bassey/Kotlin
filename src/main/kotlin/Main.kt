e64607e8a97a29237aab653dd5b343c981c9bb31
oddy-bassey <oddybassey@gmail.com>
2024-02-12 23:51:34 +0100

    sequence

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,52 +1,30 @@
 
 fun main(): Unit {
     /*
-     * Working with collections
+     * Sequences
+     * what they are?
+     * when to use it?
      */
 
-    val readOnlyList = listOf(1, 2, 3)
-    val readOnlySet = setOf(3, 4, 5)
-    val readOnlymap = mapOf(1 to "a", 2 to "b")
+    /*
+     * Sequences are iterable container providing much the same functionalities as iterable
+     * which is what backs most of our common collection types but sequences have different
+     * performance traits. Rather than eagerly applying a transformation to every item in
+     * the iterable container, sequences apply all processing steps to each individual item.
+     *
+     * you can think of a sequence as a depth-first approach while iterables are a breadth-first
+     * approach
+     */
 
-    val mutableList = mutableListOf(43, 23, 12)
-    val mutableMap = mutableMapOf(1 to "first", 2 to "second", 3 to "third")
-    val mutableSet = mutableSetOf(8, 7, 6)
+    val languages = listOf("Kotlin", "C++", "Java")
+        .filter { it.length >1 }
+        .map { it.length }
 
-    println(readOnlyList.size)
-    println(mutableList.size)
+    println(languages)
 
-    println("isempty and isnotempty method------------")
-    println(mutableSet.isEmpty())
-    println(readOnlySet.isNotEmpty())
-
-    println("printing list------------")
-    for (element in readOnlySet) {
-        println(element)
-    }
-
-    mutableMap.forEach {key, value -> println(value) }
-
-    println("first method------------")
-    println(readOnlyList.first())
-    println(mutableSet.first())
-
-    println("take method------------")
-    println(mutableList.take(3))
-    println(readOnlySet.take(2))
-
-    println("filter method------------")
-    println(mutableMap.filter { entry -> entry.key < 2})
-    println(readOnlySet.filter { value -> value > 1})
-
-    val languages = mapOf(
-        "Kotlin" to 2,
-        "Java" to 1,
-        "C++" to 3,
-        "Javascript" to 4,
-        "Python" to 5
-    )
-    println(languages.filter { it.value<= 4 })
-    println(languages.filter { it.value<= 4 }
-        .map { it.key }
-        .sorted())
+    val languageSequence = sequenceOf("Java", "Kotli", "C++", "Python")
+    val fromIterable = listOf(213, 43, 35, 54).asSequence()
+    languageSequence.filter { it.length > 4}
+        .map { it.length }
+        .take(2)
 }
