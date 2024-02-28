f59f4092c7e2a0795feb3df5f23b59f876115f69
oddy-bassey <oddybassey@gmail.com>
2024-02-28 09:07:58 +0100

    sequences

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,22 @@
 
 fun main(): Unit {
     /*
-     * Sequences
-     * what they are?
-     * when to use it?
-     */
-
-    /*
-     * Sequences are iterable container providing much the same functionalities as iterable
-     * which is what backs most of our common collection types but sequences have different
-     * performance traits. Rather than eagerly applying a transformation to every item in
-     * the iterable container, sequences apply all processing steps to each individual item.
+     * Sequences?
+     * These are types that provide similar features to other collections but
+     * processes data in a lazy and efficient manner
      *
-     * you can think of a sequence as a depth-first approach while iterables are a breadth-first
-     * approach
+     * when to use it?
+     * This lazy approach can be useful for processing collections, especially
+     * when working with large amounts of data
      */
-
     val languages = listOf("Kotlin", "C++", "Java")
-        .filter { it.length >1 }
+        .filter { it.length > 4 }
         .map { it.length }
 
-    println(languages)
+    // creating a sequence
+    val languageSequence = sequenceOf("Rust", "Python", "Javascript")
+    val fromIterable = listOf(1, 2, 3).asSequence();
+    languageSequence.filter { it.length > 4 }.map { it.length }.take(2)
 
-    val languageSequence = sequenceOf("Java", "Kotli", "C++", "Python")
-    val fromIterable = listOf(213, 43, 35, 54).asSequence()
-    languageSequence.filter { it.length > 4}
-        .map { it.length }
-        .take(2)
+    languageSequence.forEach { println(it) }
 }
