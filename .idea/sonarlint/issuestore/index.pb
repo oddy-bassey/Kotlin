e8f56e52fe2e636bdb01834413a945a32999be39
oddy-bassey <oddybassey@gmail.com>
2024-03-02 16:48:09 +0100

    assignment

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,14 @@ G
 src/main/kotlin/Main.kt,d/8/d83ae3847b7d408136086cc5e9e95f5f7c126ed5
 9
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+A
+sorted-scores.txt,6/d/6d2bd535dcf803105b3b2477841657f3abe608ba
+:
+
+scores.txt,6/3/6319cf465e43ca50c5c28921de75c6289db1a4ad
+B
+src/ass-scores.txt,f/b/fb70aba671145069c0903a555633e53cd643a411
+E
+sorted-ass-scores.txt,3/7/374b4633c666f845afcd503b0efebc0437f5fa90
+B
+highest-scores.txt,6/7/6777897743fb246d4c17b489077cf41806310e97
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,28 @@ import java.io.File
 import kotlin.io.path.writeLines
 
 fun main(): Unit {
-    val scoresFile = File("src/scores.txt")
-    scoresFile.forEachLine { line -> println(line) }
+    doAssignment()
+}
+fun doAssignment(): Unit {
+    print("Enter the name of the file: ")
+    val filename = readln()
 
-    val sortedLines = scoresFile.readLines().sorted()
-    val outputFile = File("sorted-scores.txt").toPath()
-    outputFile.writeLines(sortedLines)
+    if(filename.isNullOrEmpty()) {
+        print("Invalid input!")
+        return
+    }
+
+    val scoresFile = File("src/$filename.txt")
+    if(!scoresFile.isFile) return
+
+    val fileData = scoresFile.readLines()
+    val scoreMap = fileData.map{
+            val grade = it.split(":")
+            grade[0] to grade[1]
+        }
+    val highestScoreMap = scoreMap.sortedByDescending { it.second }.take(3)
+    val highestScores = highestScoreMap.map { "${it.first}:${it.second}" }
+
+    val outputFile = File("highest-scores.txt").toPath()
+    outputFile.writeLines(highestScores)
 }
