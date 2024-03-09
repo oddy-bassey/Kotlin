db1349522188b1c0fd5c38f622d9acf7d7c9baaa
oddy-bassey <oddybassey@gmail.com>
2024-03-09 11:33:18 +0100

    interfaces

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,39 @@
+/*
+ while we can provide default implementations for interface methods,
+ we cannot provide default values for interface properties.
+ */
+interface StringProvider {
+    val placeHolder: String
+    fun getString(id: Int): String = id.toString()
+}
+
+interface ResourceProvider: StringProvider {
+    fun getDimension(id: Int): Long = id.toLong()
+}
+
+class DefaultResourceProvider: ResourceProvider {
+    override val placeHolder: String = "<placeholder>"
+    /* the default methods can be overridden or not by the implementing class
+
+    override fun getString(id: Int): String {
+        return "id: $id"
+    }
+
+    override fun getDimension(id: Int): Long {
+        return id.toLong()
+    }
+    */
+}
 
 fun main(): Unit {
+    val someVariable: Any? = null
 
-    val person = Student(21, "Danny", "Mike", 'M')
-    print(person.age)
-    print(person.firstName)
-    print(person.lastName+"\n")
-    person.printName()
+    if(someVariable is StringProvider) {
+        println("The variable is StringProvider")
+    }
+
+    val streamProvider: DefaultResourceProvider = DefaultResourceProvider()
+    println("Id: ${streamProvider.getString(340)}")
+    println("Dimension: ${streamProvider.getDimension(322)}")
+    println(streamProvider.placeHolder)
 }
--- src/main/kotlin/Student.kt
+++
@@ -1,9 +0,0 @@
-class Student(
-    age: Int,
-    firstName: String,
-    lastname: String,
-    gender: Char): Person(age, firstName, lastname, gender) {
-    override fun printName() {
-        println("My name is Mr $firstName $lastName")
-    }
-}
