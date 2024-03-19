634518411f644c1c915eb729f4e652864d0d135f
oddy-bassey <oddybassey@gmail.com>
2024-03-20 00:17:18 +0100

    trailing lambda

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,15 +3,5 @@ G
 src/main/kotlin/Main.kt,d/8/d83ae3847b7d408136086cc5e9e95f5f7c126ed5
 9
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
-G
-src/main/kotlin/Task.kt,4/2/42802bb797a1bd1d5f88aea2aa28325e59005815
-Q
-!src/main/kotlin/NetwsorkConfig.kt,2/3/23717ce061c4d57c51708529f3174462a75aefe8
-P
- src/main/kotlin/NetworkConfig.kt,6/a/6ad0103edd58f76e50d06b2aa0dc9a279d7e8153
-V
-&src/main/kotlin/NetworkInfoProvider.kt,1/a/1ae0e2dad8ca07e56d6ec82f22cd9f32b3e76a96
-J
-src/main/kotlin/UIState.kt,2/d/2d92eba379d8f45f8366a218d929d7d745e28de7
 S
 #src/main/kotlin/SearchController.kt,a/3/a3e4da378fef2ecf940513b24bd7f2b4d1a8db61
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,16 @@
+import javax.annotation.processing.Messager
 
-/* Companion Object
- * A companion object is an object decoration within a class
- * They are most useful in organising methods and properties so they
- * re only accessible by first referencing an enclosing class name.
- *
- * NOTE:
- * when deciding between a companion object or top level property or
- * function, consider whether or not the function should be globally
- * available or whether it makes more sense within a context of another
- * explicit class.
+/* Extension functions
  */
 fun main(): Unit {
+    enhanceMessage("God is good and great") {
+        add(12, 54)
+    }
+}
 
-    val controller = SearchController.create("Kotlin")
-    // or
-    val controller2 = SearchController.Companion.create("Java")
+// val lambdaName : Type = {parameterList -> codeBody}
+val add: (Int, Int) -> Int = {a,b -> a + b}
+
+fun enhanceMessage(message: String, funAsParam: () -> Int) {
+    println("$message ${funAsParam()}")
 }
